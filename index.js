48e5b2bc91cacecd6609ff79fe919d99ed993c3e
Vikas Kushwaha <vikaskushwaha40@gmail.com>
2023-01-07 21:26:17 +0530

    db connection established

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-
 const candidates = require('./routes/candidates');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 const express = require('express');
+dotenv.config({path : './config/config.env'});
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+
 app.use('/api/candidates', candidates);
 
 const port = process.env.PORT || 3000;
