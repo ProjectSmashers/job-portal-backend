70f9518609a436320d9306185a91fb6e033ee07b
harshmaurya09 <harshm5508@gmail.com>
2023-01-14 17:24:40 +0530

    Job Pref and Prev Emp

--- controllers/updateJobPreference.js
+++ controllers/updateJobPreference.js
@@ -4,10 +4,31 @@ const updateJobPrefrence= async (req, res, next) => {
     const uniqueID = req.params.id
     console.log(uniqueID)
     try {
+        const candidate = await Candidate.updateMany({ _id: uniqueID },
+        {
+            $set: {            
+                'jobPreference.location' : req.body.jobPreference.location,
+                'jobPreference.modeOfJob' : req.body.jobPreference.modeOfJob            
+            }
+        },
+        { new: true, useFindAndModify: false, upsert: true }
+    );
 
-    }
-
-    catch {
-        
-    }
+    console.log(candidate);
+    res.status(200).json({
+        statusCode: 1,
+        message: 'Updated',
+        data: candidate
+    })
 }
+catch (err) {
+    console.error('Some error occured');
+    res.status(500).json({
+        statusCode: 0,
+        message: 'Failed',
+    })
+    console.error(err);
+} 
+}
+
+module.exports = updateJobPrefrence;
