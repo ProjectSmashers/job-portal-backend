e50510a0e8a9dd837c160dc71f3c43da250035bb
harshmaurya09 <harshm5508@gmail.com>
2023-01-23 18:29:39 +0530

    updateCompany without job description

--- controllers/updateCompany.js
+++ controllers/updateCompany.js
@@ -3,33 +3,34 @@ const Company = require("../model/Company");
 const updateCompany = async (req, res, next) => {
     const companyId = req.params.id;
     console.log(companyId);
-    let updateData = {};
+    const updateData = req.body;
+    let updateobject = {};
     try {
 
          if (req.body.hasOwnProperty("companyName")) {
-            updateData.name = req.body.companyName;
+            updateobject.companyName = req.body.companyName;
          }
-   
+
          if (req.body.hasOwnProperty("address")) {
-           updateData.address = req.body.address;
+            updateobject.companyName = req.body.address;
          }
-   
+
          if (req.body.hasOwnProperty("headCount")) {
-           updateData.headCount = req.body.headCount;
-         }
+            updateobject.headCount = req.body.headCount;
+            }
  
          if (req.body.hasOwnProperty("hrContactDetail")) {
-         updateData.hrContactDetail = req.body.hrContactDetail;
+            updateobject.hrContactDetail = req.body.hrContactDetail;
          }
    
-        Company.findOneAndUpdate({ _id: req.params.id }, updateData,
-                            { new: true, useFindAndModify: false });
+        await Company.findOneAndUpdate({ _id: companyId }, {$set :updateobject},
+                                       { new: true, useFindAndModify: false });
 
         console.log(updateData);
         res.status(200).json({
             statuscode: 1,
             message: "Updated",
-            data: updateData
+            data: updateobject
         })
     }
 
